49f6854c1066702e6cb911ff64f0566af5992f47
Hector Trillo <htrillo95@gmail.com>
2025-03-23 15:48:29 -0400

    updated api.js with headers added

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,37 +2,41 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
 
+const headers = {
+  "Content-Type": "application/json"
+};
+
 export const registerUser = async (username, password) => {
-  return await axios.post(`${BASE_URL}/register`, { username, password });
+  return await axios.post(`${BASE_URL}/register`, { username, password }, { headers });
 };
 
 export const loginUser = async (username, password) => {
-  return await axios.post(`${BASE_URL}/login`, { username, password });
+  return await axios.post(`${BASE_URL}/login`, { username, password }, { headers });
 };
 
 export const fetchAccounts = async (username) => {
-  return await axios.get(`${BASE_URL}/accounts`, { params: { username } });
+  return await axios.get(`${BASE_URL}/accounts`, { params: { username }, headers });
 };
 
-export const addPassword = async (username, account, password) => {
-  return await axios.post(`${BASE_URL}/passwords`, { username, account, password });
+export const addPassword = async (username, account_name, password) => {
+  return await axios.post(`${BASE_URL}/passwords`, { username, account_name, password }, { headers });
 };
 
 export const retrievePassword = async (username, account) => {
-  return await axios.get(`${BASE_URL}/passwords`, { params: { username, account } });
+  return await axios.get(`${BASE_URL}/passwords`, { params: { username, account }, headers });
 };
 
 export const updatePassword = async (username, account_name, password) => {
   return await axios.put(`${BASE_URL}/passwords/${account_name}`, {
     username,
-    password,
-  });
+    password
+  }, { headers });
 };
 
 export const updateUsername = async (current_username, new_username) => {
   return await fetch(`${BASE_URL}/update-username`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    headers,
     body: JSON.stringify({ current_username, new_username }),
   });
 };
@@ -40,13 +44,14 @@ export const updateUsername = async (current_username, new_username) => {
 export const deletePassword = async (username, accountName) => {
   return await axios.delete(`${BASE_URL}/passwords/${accountName}`, {
     data: { username },
+    headers
   });
 };
 
 export const deleteUserAccount = async (username) => {
   return await fetch(`${BASE_URL}/delete-account`, {
     method: "DELETE",
-    headers: { "Content-Type": "application/json" },
+    headers,
     body: JSON.stringify({ username }),
   });
 };
