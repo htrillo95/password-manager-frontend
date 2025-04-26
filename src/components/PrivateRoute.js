4b7af55f086d651854e911f9bfa0439488927fb8
Hector Trillo <htrillo95@gmail.com>
2025-04-26 17:01:12 -0400

    Deleted PrivateRoute unused

--- src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ component: Component }) => {
-  const token = localStorage.getItem("loggedInUser"); // Check if user is logged in
-  return token ? <Component /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
